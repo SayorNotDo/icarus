50cf41ae591b3704352a68b4d7f4cab24c800fb2
sayornotdo <jcskye07@hotmail.com>
2022-10-31 11:11:25 +0800

    modify model for user

--- user/models.go
+++ user/models.go
@@ -7,25 +7,25 @@ import (
 
 type User struct {
 	UID            int64     `json:"uid" gorm:"primaryKey"`
-	Username       string    `json:"username" gorm:"unique; not null"`
+	Username       string    `json:"username" gorm:"unique; not null; type:varchar(32)"`
 	HashedPassword []byte    `json:"-" gorm:"-"`
-	ChineseName    string    `json:"chineseName"`
+	ChineseName    string    `json:"chineseName" gorm:"type:varchar(32)""`
 	RoleId         int8      `json:"roleId" gorm:"default:5"`
-	EmployeeId     string    `json:"employeeId" gorm:"<-:false"`
-	Position       string    `json:"position"`
-	Email          string    `json:"email"`
-	Phone          string    `json:"phone"`
+	EmployeeId     string    `json:"employeeId" gorm:"<-:false; type:varchar(32)"`
+	Position       string    `json:"position" gorm:"type:varchar(255)"`
+	Email          string    `json:"email" gorm:"type:varchar(255)"`
+	Phone          string    `json:"phone" gorm:"type:varchar(32)"`
 	JoinDate       time.Time `json:"joinDate" gorm:"autoCreateTime"`
 	LastLoginTime  time.Time `json:"lastLoginTime" gorm:"autoUpdateTime:milli"`
 	Status         bool      `json:"status" gorm:"default:1"`
-	Department     string    `json:"department"`
+	Department     string    `json:"department" gorm:"type:varchar(255)"`
 }
 
 type Department struct {
 	DID     int16  `json:"did" gorm:"primaryKey"`
-	Name    string `json:"name" gorm:"not null"`
-	Center  string `json:"center"`
-	Company string `json:"company" gorm:"not null"`
+	Name    string `json:"name" gorm:"not null; type:varchar(255)"`
+	Center  string `json:"center" gorm:"type:varchar(255)"`
+	Company string `json:"company" gorm:"not null; type:varchar(255)"`
 }
 
 func GeneratePassword(password string) ([]byte, error) {
