8fb8a8bf996acbb940b7ab8d278958464d29e165
sayornotdo <jcskye07@hotmail.com>
2022-11-16 22:48:46 +0800

    add trino

--- database/trino/connect.go
+++ database/trino/connect.go
@@ -1,1 +1,20 @@
 package trino
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/trinodb/trino-go-client/trino"
+)
+
+var TrinoDb *sql.DB
+
+func init() {
+	dsn := ""
+	TrinoDb, err := sql.Open("trino", dsn)
+	if err != nil {
+		log.Printf("connect error: %v", err)
+		return
+	}
+	log.Println(TrinoDb)
+}
